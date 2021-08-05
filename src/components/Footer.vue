c53159e6ff1bffeb3af40a148456c6512f81bac4
ScottTomassetti <scott_t_ct@hotmail.com>
2021-08-05 12:30:37 -0400

    Initial commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="footer">
+     <nav>
+      <h3>@T-Apps</h3>
+     </nav>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .footer {
+    padding: 16px 10px;
+    margin-bottom: 20px;
+    margin-top: 30px;
+    background: rgb(129, 211, 197);
+  }
+  nav {
+    display: flex;
+    align-items: center;
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+</style>
---
+++ src/firebase/config.js
@@ -0,0 +1,27 @@
+  
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+import 'firebase/storage'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC2T3FH9UJGcDv5Uxl7xmiK6mUz2BVDqLk",
+  authDomain: "bike-maintain.firebaseapp.com",
+  projectId: "bike-maintain",
+  storageBucket: "bike-maintain.appspot.com",
+  messagingSenderId: "798176927210",
+  appId: "1:798176927210:web:bfa9a4d32268ab1e67e693"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+const projectStorage = firebase.storage()
+
+// timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, projectAuth, projectStorage, timestamp }
