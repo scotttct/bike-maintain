3dd574cef358ca87fa1730aa2b9d9e3243de57b2
Richard Tomassetti <55094377+scotttct@users.noreply.github.com>
2021-08-06 08:19:45 -0400

    Update config.js
    Remove Firebase keys

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,12 +5,7 @@ import 'firebase/auth'
 import 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC2T3FH9UJGcDv5Uxl7xmiK6mUz2BVDqLk",
-  authDomain: "bike-maintain.firebaseapp.com",
-  projectId: "bike-maintain",
-  storageBucket: "bike-maintain.appspot.com",
-  messagingSenderId: "798176927210",
-  appId: "1:798176927210:web:bfa9a4d32268ab1e67e693"
+  Place your own keys here
 };
 
 // init firebase
