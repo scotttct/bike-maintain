676dd269313513f5a5e7329ccddb9747950e336f
ScottTomassetti <scott_t_ct@hotmail.com>
2021-08-06 10:08:49 -0400

    update gitignore file

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -6,13 +6,7 @@ import 'firebase/storage'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC2T3FH9UJGcDv5Uxl7xmiK6mUz2BVDqLk",
-  authDomain: "bike-maintain.firebaseapp.com",
-  projectId: "bike-maintain",
-  storageBucket: "bike-maintain.appspot.com",
-  messagingSenderId: "798176927210",
-  appId: "1:798176927210:web:bfa9a4d32268ab1e67e693",
-  measurementId: "G-LC399RQ80T"
+//  Place your Firebase configuration here
 };
 
 // init firebase
