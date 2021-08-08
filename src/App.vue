d70fcba13a18ded5e42bfdca8689664b2e496886
ScottTomassetti <scott_t_ct@hotmail.com>
2021-08-08 14:14:35 -0400

    Made site more responsive while in mobile view

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <Navbar />
+  <MobileNav v-if="mobileView"/>
+  <Navbar v-if="!mobileView"/>
   <div class="content">
     <router-view/>
   </div>
@@ -9,8 +10,24 @@
 <script>
 import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue'
+import MobileNav from './components/MobileNav.vue'
 export default {
-  components: { Navbar, Footer }  
+  data: () => {
+    return {
+      mobileView: true,
+      showNav: false
+    };
+  },
+  methods: {
+    handleView() {
+      this.mobileView = window.innerWidth <= 650;
+    }
+  },
+  components: { Navbar, MobileNav, Footer },
+  created() {
+    this.handleView();
+    window.addEventListener('resize', this.handleView);
+  }
 }
 </script>
 
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,7 +14,7 @@ export default {
 
 <style scoped>
     .footer {
-    position: fixed;
+    position: relative;
     left: 0;
     bottom: 0;
     width: 100%;
